2df1c831d2817ea823571ea0783e68a8d519756a
AiSaurabhpatil <aientrepreneur15@gmail.com>
2023-08-22 13:11:43 +0530

    minor fixes

--- components/audio_to_text.py
+++ components/audio_to_text.py
@@ -1,12 +1,12 @@
+import os
 from whisper_jax import FlaxWhisperPipline
-import os 
+from tqdm import tqdm  # Import tqdm for the progress bar
 
-# Instantiate the pipeline
+# Instantiate the Whisper pipeline
 pipeline = FlaxWhisperPipline("openai/whisper-base")
 
-# List of audio files to transcribe
+# Input directory containing audio files
 input_directory = "../video_to_mp3"
-audio_files = [os.path.join(input_directory, file) for file in os.listdir(input_directory) if file.endswith(".mp3")]
 
 # Output directory for the transcriptions
 output_directory = "../transcriptions"
@@ -14,12 +14,20 @@ output_directory = "../transcriptions"
 # Create the output directory if it doesn't exist
 os.makedirs(output_directory, exist_ok=True)
 
+# List audio files in the input directory
+audio_files = [os.path.join(input_directory, file) for file in os.listdir(input_directory) if file.endswith(".mp3")]
+
+# Initialize tqdm to display the progress bar for transcribing audio files
+progress_bar_audio = tqdm(total=len(audio_files), desc="Transcribing Audio")
+
 # Loop through each audio file and transcribe
 for index, audio_file in enumerate(audio_files, start=1):
     try:
         # Perform ASR (Automatic Speech Recognition)
-        transcription = pipeline(audio_file, task="transcribe")
-        transcription_text = transcription["text"]
+        transcriptions = pipeline(audio_file, task="transcribe")
+
+        # Extract the transcription text as a string
+        transcription_text = transcriptions["text"]
 
         # Save the transcription to a text file
         transcription_file = os.path.join(output_directory, f"transcription_{index}.txt")
@@ -27,7 +35,23 @@ for index, audio_file in enumerate(audio_files, start=1):
         with open(transcription_file, "w", encoding="utf-8") as text_file:
             text_file.write(transcription_text)
 
-        print(f"Audio {index}: Transcription saved to {transcription_file}")
+        # Update the progress bar for audio file processing
+        progress_bar_audio.update(1)
+        progress_bar_audio.set_postfix({"File": audio_file})
 
     except Exception as e:
         print(f"Audio {index}: An error occurred while processing {audio_file}: {str(e)}")
+
+# Close the progress bar for audio file processing
+progress_bar_audio.close()
+
+# Now, you can create a progress bar for the overall transcription process
+progress_bar_transcription = tqdm(total=len(audio_files), desc="Transcribing Progress")
+
+# Loop through each audio file again to update the overall progress bar
+for _ in audio_files:
+    # Update the progress bar for the overall transcription process
+    progress_bar_transcription.update(1)
+
+# Close the progress bar for the overall transcription process
+progress_bar_transcription.close()
